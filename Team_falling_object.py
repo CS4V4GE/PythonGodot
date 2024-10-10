aa9f9d74e783ada9c44be161190f903e001a47f3
StocktonNelson <stocktonnelson@byui.edu>
2024-10-10 14:52:38 -0600

    My Final Update!!!

--- Team_falling_object.py
+++ Team_falling_object.py
@@ -20,12 +20,27 @@ c = (1 / 2) * p * A * C
 velocity = math.sqrt((m*g)/c) * (1 - math.exp((-math.sqrt(m*g*c)/m)*t))
 earth_velocity = math.sqrt((m*9.8)/c) * (1 - math.exp((-math.sqrt(m*9.8*c)/m)*t))
 jupiter_velocity = math.sqrt((m*24)/c) * (1 - math.exp((-math.sqrt(m*24*c)/m)*t))
-v_terminal = math.sqrt((m*g)/c)
+
+
+#according to google search it said that cross-sectional area of flat is 0.45 and head first is 0.18.
+#that is how i found this number if you ask me.
+flat_skydiver = math.sqrt((m*g)/0.45) * (1 - math.exp((-math.sqrt(m*g*0.45)/m)*t))
+head_first_skydiver = math.sqrt((m*g)/0.18) * (1 - math.exp((-math.sqrt(m*g*0.18)/m)*t))
+
 
 #this is the prints
 print(f"\nThe inner value of c is: {c:.3f}")
 print(f"The velocity after {t} seconds is: {velocity:.3f} m/s")
-print(f"Velocity and {t} with earth gavity: {earth_velocity:.3f} m/s")
-print(f"Velocity and {t} with earth gavity: {jupiter_velocity:.3f} m/s")
-print(f"The terminal velocity is: {v_terminal:.3f}")
-
+print(f"Velocity and {t} seconds with earth gavity: {earth_velocity:.3f} m/s")
+print(f"Velocity and {t} seconds with earth gavity: {jupiter_velocity:.3f} m/s")
+print(f"Velocity and {t} seconds of falt skydiver: {flat_skydiver:.3f} m/s")
+print(f"Velocity and {t} seconds with headfirst skydiver: {head_first_skydiver:.3f} m/s")
+user_input = input("What termianal velocity do you want to see? (flat or head)")
+if (user_input == "falt"):
+    v_terminal = math.sqrt((m*g)/0.45)
+    print(f"The terminal velocity of flat skydiver is: {v_terminal:.3f}")
+elif (user_input == "head"):
+    v_terminal = math.sqrt((m*g)/0.18)
+    print(f"The terminal velocity of flat skydiver is: {v_terminal:.3f}")
+else:
+    print("miss input")
